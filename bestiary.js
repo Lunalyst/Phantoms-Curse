73ff0b29e9a9606ddc4fd021370e2cd55e66453d
lunalyst <141511481+Lunalyst@users.noreply.github.com>
2023-08-05 10:42:43 -0700

    Add files via upload

---
+++ bestiary.js
@@ -0,0 +1,126 @@
+//https://stackoverflow.com/questions/71266893/phaser-3-change-hitbox-interactive-area-of-sprite-without-physics
+
+let bestiaryThat;
+class bestiary extends Phaser.Physics.Arcade.Sprite{
+    // every class needs constructor
+    constructor(scene, xPos, yPos){
+      //super() calls the constructor() from the parent class we are extending
+      super(scene, xPos, yPos, 'bestiary');
+      //then we add new instance into the scene. when ising this inside a class definition is refering to the instance of the class
+      //so here in the subclass of sprite its refering to the image object we just made. 
+      scene.add.existing(this);
+
+      this.anims.create({key: 'closed',frames: this.anims.generateFrameNames('bestiary', { start: 0, end: 0 }),frameRate: 10,repeat: -1});
+      this.anims.create({key: 'p0',frames: this.anims.generateFrameNames('bestiary', { start: 1, end: 1 }),frameRate: 10,repeat: -1});
+      this.anims.create({key: 'p1',frames: this.anims.generateFrameNames('bestiary', { start: 2, end: 2 }),frameRate: 10,repeat: -1});
+      this.anims.create({key: 'p2',frames: this.anims.generateFrameNames('bestiary', { start: 3, end: 3 }),frameRate: 10,repeat: -1});
+      this.anims.create({key: 'p3',frames: this.anims.generateFrameNames('bestiary', { start: 4, end: 4 }),frameRate: 10,repeat: -1});
+      this.anims.create({key: 'p4',frames: this.anims.generateFrameNames('bestiary', { start: 5, end: 5 }),frameRate: 10,repeat: -1});
+      this.anims.create({key: 'p5',frames: this.anims.generateFrameNames('bestiary', { start: 6, end: 6 }),frameRate: 10,repeat: -1});
+      this.anims.create({key: 'p6',frames: this.anims.generateFrameNames('bestiary', { start: 7, end: 7 }),frameRate: 10,repeat: -1});
+      this.anims.play("closed");
+
+      this.setDepth(60);
+      this.setScale(1.5 );
+      //connects the sprite to the camera so that it sticks with the player.
+      this.setScrollFactor(0);
+      this.isOpen = false;
+      this.index = 0;
+      this.visable = false;
+      this.openDelay = false;
+      bestiaryThat = this;
+      this.pageNumber = 0;
+     
+      
+    }
+
+    openBestiary(scene){
+         
+        if(this.isOpen === false && this.openDelay === false){
+            this.isOpen = true;
+            console.log("this.isOpen from bestiary"+this.isOpen);
+            this.anims.play("p"+this.pageNumber);
+            this.openDelay = true;
+            
+            setTimeout(function(){
+              bestiaryThat.openDelay = false; 
+              },1000);
+              if(this.pageNumber === 0){
+                scene.bestiaryLeft.visible = false;
+                scene.bestiaryRight.visible = true;
+                //scene.bestiaryExit.visible = true;
+              }else if(this.pageNumber === 6){
+                scene.bestiaryLeft.visible = true;
+                scene.bestiaryRight.visible = false;
+                //scene.bestiaryExit.visible = true;
+              }else{
+                scene.bestiaryLeft.visible = true;
+                scene.bestiaryRight.visible = true;
+                //scene.bestiaryExit.visible = true;
+              }
+
+        }else if(this.isOpen === true && this.openDelay === false){
+            this.isOpen = false;
+            console.log("this.isOpen from bestiary"+this.isOpen);
+            this.anims.play("closed");
+            this.openDelay = true;
+            setTimeout(function(){
+              console.log("bestiary openDelay set to false");
+              bestiaryThat.openDelay = false; 
+              },1000);
+              scene.bestiaryLeft.visible = false;
+              scene.bestiaryRight.visible = false;
+              //scene.bestiaryExit.visible = false;
+        }
+
+    }
+
+    applyUIControlElements(scene){
+    
+      //scene.bestiaryExit.on('pointerdown', function (pointer) {
+        /*this.isOpen = false;
+        this.anims.play("closed");
+        this.openDelay = true;
+        setTimeout(function(){
+          console.log("bestiary openDelay set to false");
+          bestiaryThat.openDelay = false; 
+          },1000);*/
+        
+        
+      //});
+      scene.bestiaryRight.on('pointerdown', function (pointer) {
+        console.log(" activating bestiary turn page right. scene.bestiaryUI.pageNumber" + scene.bestiaryUI.pageNumber);
+        if(scene.bestiaryUI.pageNumber >= 0 && scene.bestiaryUI.pageNumber < 6 ){
+          scene.bestiaryUI.pageNumber++;
+          scene.bestiaryUI.anims.play("p"+scene.bestiaryUI.pageNumber);
+          if(scene.bestiaryUI.pageNumber === 6){
+            console.log(" hiding right bestiary arrow" );
+              scene.bestiaryRight.visible = false;
+          }else{
+            scene.bestiaryLeft.visible = true;
+            scene.bestiaryRight.visible = true;
+          }
+        }
+
+        
+        
+      });
+      scene.bestiaryLeft.on('pointerdown', function (pointer) {
+        console.log(" activating bestiary turn page left scene.bestiaryUI.pageNumber" + scene.bestiaryUI.pageNumber);
+        if(scene.bestiaryUI.pageNumber > 0 && scene.bestiaryUI.pageNumber <= 6 ){
+          scene.bestiaryUI.pageNumber--;
+          scene.bestiaryUI.anims.play("p"+scene.bestiaryUI.pageNumber);
+          if(scene.bestiaryUI.pageNumber === 0){
+            console.log(" hiding left bestiary arrow" );
+            scene.bestiaryLeft.visible = false;
+          }else{
+            scene.bestiaryLeft.visible = true;
+            scene.bestiaryRight.visible = true;
+          }
+        }
+        
+        
+      });
+   
+    }
+}
---
+++ gameOver.js
@@ -0,0 +1,81 @@
+/*
+https://phaser.io/examples/v3/view/input/mouse/click-sprite
+https://phaser.io/examples/v2/input/pointer-over
+ */
+class gameOver extends Phaser.Scene {
+    constructor(){
+        // scene settings
+        super({key: 'gameOverForest',active: false,physics:{default:'arcade'}});
+        //variables attached to the scene
+        this.newGame;
+        this.processMap;
+        this.enemy;
+        this.gameOverSign;
+        this.tryAgain;
+        this.allFunctions;
+        }
+
+        preload(){
+            this.load.image("gameOverbackground" , "assets/titleScreenBackground.png");
+            this.load.image("source_map" , "assets/tiledMap/Tile Set V.0.8.png");
+            this.load.image("gameOverSign" , "assets/gameoversign.png");
+            this.load.image("tryAgianSign" , "assets/try agian.png");
+             //load in the JSON file for the bitmap
+            this.load.tilemapTiledJSON("mapGameover" , "assets/tiledMap/gameOverForest.json");
+            this.load.spritesheet('blueSlime', 'assets/CommonBlueSlime.png',{frameWidth: 100, frameHeight: 100 });
+           
+            
+        }
+
+        create(){
+            console.log("now in gameover scene");
+            let backround = this.add.sprite(450, 0, "gameOverbackground");
+            backround.setScale(1.5,1.5);
+            this.tryAgian = this.add.sprite(450, 450, "tryAgianSign").setInteractive();
+            this.tryAgian.setDepth(7);
+            console.log("loading gameover tileset");
+            let myMap = this.make.tilemap({ key: "mapGameover" });
+            //creates a new level object which is used to display map. sends scene and mapdata
+            this.processMap = new level(this,myMap);
+            //calls function that loads the tiles from the json
+            this.processMap.tilesetNameInTiled = "Tile Set V.0.8";
+            this.processMap.setTiles();
+            this.enemy = new blueSlime(this,450, 295);
+            this.enemy.slimeGameOver();
+            this.physics.add.collider(this.processMap.layer1, this.enemy);
+            //sets up camera to follow player.
+            this.mycamera = this.cameras.main;
+            this.mycamera.startFollow(this.enemy);
+            this.mycamera.setBounds( 0, 0, myMap.widthInPixels, myMap.HeightInPixels); 
+            this.cameras.main.zoom = 1.7;
+            this.gameOverSign = this.add.sprite(450,100,"gameOverSign");
+            this.gameOverSign.setDepth(7);
+            let that = this;
+            this.allFunctions = new allSceneFunctions;
+            /*
+            this.anims.create({key: 'newActive',frames: this.anims.generateFrameNames('newGame', { start: 1, end: 1 }),frameRate: 1,repeat: -1});
+            this.anims.create({key: 'newInActive',frames: this.anims.generateFrameNames('newGame', { start: 0, end: 0 }),frameRate: 1,repeat: -1});
+            let backround = this.add.sprite(450, 450, "background");
+            backround.setScale(1.5,1.5);
+            this.newGame =  this.add.sprite(460, 500, "newGame").setInteractive();
+            this.newGame.setScale(3,3);
+            let loadGame =  this.add.sprite(460, 550, "loadGame").setInteractive();
+            loadGame.setScale(3,3);
+
+            this.input.mouse.capture = true;*/
+            // adds a try agian buton that loads the player back into a scene if they die.
+            this.tryAgian.on('pointerdown', function (pointer) {
+
+                that.allFunctions.saveGame(1650,542,6,[2,4,6,8,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+                that.scene.start('forestHome');
+        
+            });
+            
+
+        }
+
+        update(){
+            
+        }
+
+}

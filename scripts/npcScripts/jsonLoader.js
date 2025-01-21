b3d95f774fad82383a71e53fc1864770f99cbfad
lunalyst <141511481+Lunalyst@users.noreply.github.com>
2025-01-20 19:16:58 -0800

    save

--- scripts/npcScripts/jsonLoader.js
+++ scripts/npcScripts/jsonLoader.js
@@ -1,6 +1,5 @@
-
 //import our dialogue json file 
-//import npcDialogue from './dialogue/npcDialogue.json' assert{type: 'json'};
+import npcDialogue from './dialogue/npcDialogue.json' assert{type: 'json'};
 
 
 

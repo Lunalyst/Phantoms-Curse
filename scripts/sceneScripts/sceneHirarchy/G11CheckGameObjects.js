c56d411998f5e150b8acdb481e945ca963c5578f
lunalyst <141511481+Lunalyst@users.noreply.github.com>
2025-11-08 23:17:47 -0800

    saveing cloud fix

--- scripts/sceneScripts/sceneHirarchy/G11CheckGameObjects.js
+++ scripts/sceneScripts/sceneHirarchy/G11CheckGameObjects.js
@@ -329,8 +329,7 @@ class G11CheckGameObjects extends G10CheckNPCS {
         tempProjectile.setVelocityX(0);
       }
       
-     
-
+    
       /*//overlap function to let player know that the curse bar is increasing.
       tempScene.physics.add.overlap(tempScene.player1.mainHitbox, tempProjectile, function () {
 
